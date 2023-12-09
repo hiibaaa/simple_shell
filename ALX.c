d20a6a0c72bdec500df7d218ccec55de59fc0624
hiibaaa <hibajamalelarab@gmail.com>
2023-12-09 11:36:50 -0800

    simple

---
+++ ALX.c
@@ -0,0 +1,87 @@
+#include "main.h"
+/**
+ *find_executable_path - function that finds the path of an executable command
+ *@command: name of the command
+ *@path: a colon-separated list of paths to search from
+ *Return: command_path or null
+ */
+char *find_executable_path(const char *command, const char *path)
+{
+	char *path_copy = strdup(path);
+	char *token = strtok(path_copy, ":");
+	char command_path[1024];
+
+	while (token != NULL)
+	{
+		sprintf(command_path, "%s/%s", token, command);
+
+		if (access(command_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (strdup(command_path));
+		}
+		token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
+}
+/**
+ *execute_command - function that executes a command with arguments
+ *@command: command to be executed
+ *@arguments: array of arguments for the command
+ *@line: pointer to the original line of command
+ */
+void execute_command(char *command, char **arguments, char **line)
+{
+	pid_t pid = fork();
+
+	line = line;
+
+	if (pid == 0)
+	{
+		if (strchr(command, '/') != NULL)
+		{
+			if (execve(command, arguments, environ) == -1)
+			{
+				perror("Error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			char *path = recreated_getenv("PATH");
+			char *command_path = find_executable_path(command, path);
+
+			if (command_path != NULL)
+			{
+				execve(command_path, arguments, environ);
+				free(command_path);
+			}
+			else
+			{
+				perror("Command not found");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("Error");
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+	}
+}
+/**
+ *print_environment - function that prints the environment
+ */
+void print_environment(void)
+{
+	char **env_ptr;
+
+	for (env_ptr = environ; *env_ptr != NULL; env_ptr++)
+	{
+		printf("%s\n", *env_ptr);
+	}
+}
