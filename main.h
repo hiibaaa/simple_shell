e21dcd334a2c4051577c24032a7235e4779c9e88
hiibaaa <hibajamalelarab@gmail.com>
2023-12-12 15:47:03 -0800

    simple

--- main.h
+++ main.h
@@ -1,22 +1,49 @@
-#ifndef MAIN_H
-#define MAIN_H
-#define MAX_COMMAND_LENGTH 1024
-#define MAX_ARGUMENTS 64
+#ifndef SHELL_H_
+#define SHELL_H_
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <limits.h>
+#include <signal.h>
 
-void display_prompt(void);
-char *read_command();
-char **pased_arguments(char *line);
-void execute_command(char *command, char **arguments, char **line);
-void print_environment(void);
-char *find_executable_path(const char *command, const char *path);
-char *recreated_getenv(const char *name);
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 extern char **environ;
+
+/** function prototypes **/
+void cant_find(char *arv, int dsh, char *comnd);
+void invalid_num(char *arv, int dsh, char *numb);
+char *fetch_env(char *env_lbl);
+void outpt_env(char **);
+void error_perm(char *arv, int dsh, char *comnd);
+
+void mk_cnvrt(int t);
+int mk_asctoint(const char *s);
+
+/*** putting chars and strings ***/
+void cpchar(char c);
+void sps(char *s);
+void ptchar(char c);
+void ps(char *s);
+
+/** utils **/
+char **tknize(char *strng);
+char *g_comnd(char *comnd);
+char *g_evr(char *env_lbl);
+void tkn_free(char **tkn);
+void ext_sh(char **tkn, char **arv, int dsh, int stats, char *bufr);
+int c_evr(char **tkn, char **env, char *bufr);
+char **g_tkn(char **bufr, int stats);
+int exe_comnd(char **tkn, char **arv, int dsh, char **env);
+
+/** strings **/
+int slen(const char *s);
+char *scopy(char *de, char *sr);
+char *scncat(char *de, char *sr);
+int scmp(char *st1, char *st2);
 
 #endif
